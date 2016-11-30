12581cc4cbd6e0d15a39d0e39353e3dd0a56aaf7
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-11-30 13:25:42 +0100

    Added put and get methods, and a test method.

--- src/kvs.c
+++ src/kvs.c
@@ -17,6 +17,38 @@ static void __exit onunload(void) {
     printk(KERN_EMERG "Loadable module removed\n");
 }
 
+void run_tests(void) {
+    int value = 5;
+    int key = 7;
+    int fetched_value = 0;
+    printk(KERN_INFO "Storing value %d with key %d.\n", value, key);
+    put(value, key);
+
+    fetched_value = get(key);
+    printk(KERN_INFO "Found value  %d for key %d.\n.", fetched_value, key);
+}
+
+//make static? also implement mem allocation here
+void put(int value, int key) {
+    kvs_entry_t kvs_entry = {
+        .value = value,
+        .key = key
+    };
+
+    hash_add(ht, &kvs_entry.next, key);
+}
+
+//make static, maybe shouldn't return -1?
+int get(int key) {
+    kvs_entry_t *kvs_entry;
+    hash_for_each_possible(ht, kvs_entry, next, key) {
+        if (kvs_entry->key == key)
+            return kvs_entry->value;
+    }
+
+    return -1;
+}
+
 module_init(onload);
 module_exit(onunload);
 
