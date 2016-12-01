7e8d35f50435608ad22fb0f8c10c452891be34eb
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-12-01 15:19:06 +0000

    Added struct definition for ht entries directly to kvs_ht.c

--- src/kvs_ht.c
+++ src/kvs_ht.c
@@ -5,22 +5,28 @@
 #include <linux/hashtable.h>
 #include "kvs_ht.h"
 
+typedef struct {
+    int key;
+    int value;
+    struct hlist_node next;
+} ht_entry_t;
+
 static DEFINE_HASHTABLE(ht, 5);
 
 void put(int value, int key) {
-    kvs_entry_t kvs_entry = {
+    ht_entry_t ht_entry = {
         .value = value,
         .key = key
     };
 
-    hash_add(ht, &kvs_entry.next, key);
+    hash_add(ht, &ht_entry.next, key);
 }
 
 int get(int key) {
-    kvs_entry_t *kvs_entry;
-    hash_for_each_possible(ht, kvs_entry, next, key) {
-        if (kvs_entry->key == key)
-            return kvs_entry->value;
+    ht_entry_t *ht_entry;
+    hash_for_each_possible(ht, ht_entry, next, key) {
+        if (ht_entry->key == key)
+            return ht_entry->value;
     }
 
     return -1;
