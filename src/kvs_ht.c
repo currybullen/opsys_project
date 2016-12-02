e7137fa197dd8d990211124ac48553c940c2ef10
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-12-02 10:21:05 +0000

    put() now returns false if value already exists. Removed static helper function.

--- src/kvs_ht.c
+++ src/kvs_ht.c
@@ -10,29 +10,19 @@ typedef struct {
 
 static DEFINE_HASHTABLE(ht, 5);
 
-static bool replace_if_present(int key, int value) {
+bool kvs_ht_put(int key, int value) {
     ht_entry_t* ht_entry;
-    hash_for_each_possible(ht, ht_entry, next, key) {
-        if (ht_entry->key == key) {
-            ht_entry->value = value;
-            return true;
-        }
-    }
+    int tmp;
 
-    return false;
-}
+    if (kvs_ht_get(key, &tmp) && tmp == value) 
+        return false;
 
-void kvs_ht_put(int key, int value) {
-    ht_entry_t* ht_entry;
-
-    if (replace_if_present(key,value))
-        return;
-    
     ht_entry = kmalloc(sizeof(ht_entry_t), GFP_KERNEL);
     ht_entry->key = key;
     ht_entry->value = value;
-
     hash_add(ht, &(ht_entry->next), key);
+
+    return true;
 }
 
 bool kvs_ht_get(int key, int* value) {
--- src/kvs_ht.h
+++ src/kvs_ht.h
@@ -1,4 +1,4 @@
-void kvs_ht_put(int key, int value);
+bool kvs_ht_put(int key, int value);
 
 bool kvs_ht_get(int key, int* value);
 
