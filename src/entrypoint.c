cd89a7e69571967c1c2acdf50745a06526f857b6
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-12-02 15:53:39 +0000

    Added skeleton methods for device open, release, read and write.

--- src/entrypoint.c
+++ src/entrypoint.c
@@ -1,26 +1,84 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include "kvs_helper.h"
 #include "kvs_ht.h"
 
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
+#define DEVICE_NAME "kvs"
+#define BUF_LEN 10
+
+static int kvs_major;
+static bool kvs_open = false;
+static char msg[BUF_LEN];
+static int msg_write_size;
+
+static struct file_operations fops = {
+    .read = device_read,
+    .write = device_write,
+    .open = device_open,
+    .release = device_release
+};
+
+static int device_open(struct inode *inode, struct file *file) {
+    return 0;
+}
+
+static int device_release(struct inode *inode, struct file *file) {
+    return 0;
+}
+
+
+static ssize_t device_read(struct file *filp, 
+                          char *buffer, 
+                          size_t length, 
+                          loff_t *offset)
+{
+    //copy from global buffer to parameter buffer
+    return 0;
+}
+
+static ssize_t device_write(struct file *filp, 
+                            const char *buff, 
+                            size_t len, 
+                            loff_t *offset) 
+{
+    switch(buff[0])
+    {
+        case KVS_PUT :
+            //handle_put(buff, len, msg);
+            break;
+
+        case KVS_GET :
+            //handle_get(buff, len, msg);
+            break;
+
+        case KVS_DEL :
+            //handle_del(buff, len, msg);
+            break;
+
+        default :
+            ;
+
+    }
+
+    return 0;
+}
+
+
 static int __init onload(void) {
+    register_chrdev(0, DEVICE_NAME, &fops);
     printk(KERN_EMERG "Loadable module initialized\n");
     return 0;
 }
 
 static void __exit onunload(void) {
     printk(KERN_EMERG "Loadable module removed\n");
-}
-
-static void run_tests(void) {
-    int value = 5;
-    int key = 7;
-    int fetched_value = 0;
-    printk(KERN_INFO "Storing value %d with key %d.\n", value, key);
-    kvs_ht_put(value, key);
-
-    kvs_ht_get(key, &fetched_value);
-    printk(KERN_INFO "Found value  %d for key %d.\n.", fetched_value, key);
 }
 
 module_init(onload);
