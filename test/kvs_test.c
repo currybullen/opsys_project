a27d8d5ef4099ac109ed1d80b51aea18aae92de6
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2016-12-05 14:46:17 +0000

    Rewrote the tests to use ioctl instead of read and write.

--- test/kvs_test.c
+++ test/kvs_test.c
@@ -3,100 +3,80 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <sys/ioctl.h>
 #include "kvs_helper.h"
 
-//#define KVS_PUT 1
-//#define KVS_PUT_OK 2
-//#define KV_PUT_FAIL 3
-//
-//#define KVS_GET 4
-//#define KVS_GET_OK 5
-//#define KVS_GET_FAIL 6
-//
-//#define KVS_DEL 7
-//#define KVS_DEL_OK 8
-//#define KVS_DEL_FAIL = 9
 
 static char* KVS_PATH = "/dev/kvs";
+static int fd;
 
-bool put(int key, int value);
-bool get(int key, int *value);
-bool delete(int key);
-void run_tests();
-
-//static void serialize(int value, char dest[], int offset);
-//static int deserialize(char src[], int offset);
-static void write_read(char write_buf[], int write_size, 
-        char read_buf[], int read_size);
+void test_put(int key, int value);
+void test_get(int key);
+void test_del(int key);
 
 int main(int argc, char *argv[]) {
     if (argc > 1)
         KVS_PATH = argv[1];
-    run_tests();
-}
-
-void run_tests() {
-    int key = 7;
-    int value = 5;
-    printf("Storing value %d with key %d\n", value, key);
-    put(key, value);
-
-    get(key, &value);
-    printf("Found value %d behind key %d\n", value, key);
-}
-
-bool put(int key, int value) {
-    char buffer[9];
-    buffer[0] = KVS_PUT;
-    serialize(key, buffer, 1);
-    serialize(value, buffer, 5);
-    write_read(buffer, 9, buffer, 9);
-
-    return (buffer[0] == KVS_PUT_OK);
-}
-
-bool get(int key, int* value) {
-    char buffer[5];
-    buffer[0] = KVS_GET;
-    serialize(key, buffer, 1);
-    write_read(buffer, 5, buffer, 5);
-    if (buffer[0] == KVS_GET_OK) {
-        *value = deserialize(buffer, 1);
-        return true;
+    fd = open(KVS_PATH, 0);
+    if (fd < 0) {
+        printf("Can't open device file %s\n", KVS_PATH);
+        exit(-1);
     }
 
-    return false;
+    test_put(99, 100);
+    test_get(99);
+    test_del(99);
+    test_get(99);
 }
 
-bool delete(int key) {
-    char buffer[5];
-    buffer[0] = KVS_DEL;
-    serialize(key, buffer, 1);
-    write_read(buffer, 5, buffer, 5);
-    
-    return (buffer[0] == KVS_DEL_OK);
+void test_put(int key, int value) {
+    kvs_msg_t msg;
+    msg.key = key;
+    msg.value = value;
+
+    printf("Attempting to store value %d behind key %d.\n", msg.key, msg.value);
+    int ret_val = ioctl(fd, IOCTL_KVS_PUT, &msg);
+    if (ret_val == KVS_SUCCESS) {
+        printf("Put call returned success!\n");
+        if (msg.status == KVS_SUCCESS)
+            printf("Returned message contained success code! Stored value %d behind key %d.\n", value, key);
+        else
+            printf("Returned message contained fault code, could not store value %d behind key %d.\n", value, key); 
+    } else {
+        printf("Put call returned %d, something is wrong.\n", ret_val); 
+    }
 }
 
-//void serialize(int value, char dest[], int offset) {
-//    dest[offset] = (value >> 24) & 0xFF;
-//    dest[offset+1] = (value >> 16) & 0xFF;
-//    dest[offset+2] = (value >> 8) & 0xFF;
-//    dest[offset+3] = value & 0xFF;
-//}
-//
-//static int deserialize(char src[], int offset) {
-//    int ret = src[offset];
-//    ret = (ret << 8) + src[offset+1];
-//    ret = (ret << 8) + src[offset+2];
-//    ret = (ret << 8) + src[offset+3];
-//
-//    return ret;
-//}
+void test_get(int key) {
+    kvs_msg_t msg;
+    msg.key = key;
 
-static void write_read(char write_buf[], int write_size, 
-        char read_buf[], int read_size) {
-    int fd = open(KVS_PATH, O_RDWR);
-    write(fd, write_buf, write_size);
-    read(fd, read_buf, read_size);
-    close(fd);
+    printf("Attempting to retrieve value behind key %d.\n", key);
+    int ret_val = ioctl(fd, IOCTL_KVS_GET, &msg);
+    if (ret_val == KVS_SUCCESS) {
+        printf("Get call returned success!\n");
+        if (msg.status == KVS_SUCCESS)
+            printf("Returned message contained success code! Retrieved value %d for key %d.\n", msg.value, key);
+        else
+            printf("Returned message contained fault code, could not retrieve value behind key %d", key); 
+    } else {
+        printf("Get call returned %d, something is wrong.\n", ret_val);
+    }
+}
+
+void test_del(int key) {
+    kvs_msg_t msg;
+    msg.key = key;
+
+    printf("Attempting to delete value behind key %d.\n", key);
+    int ret_val = ioctl(fd, IOCTL_KVS_DEL, &msg);
+    if (ret_val == KVS_SUCCESS) {
+        printf("Del call returned success!\n");
+        if (msg.status == KVS_SUCCESS)
+            printf("Returned message contained success code! Deleted value behind key %d.\n", key);
+        else
+            printf("Returned message contained fault code, could not delete value behind key %d\n", key); 
+    } else {
+        printf("Del call returned %d, something is wrong.\n", ret_val);
+    }
 }
